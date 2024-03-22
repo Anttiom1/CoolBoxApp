5591bb2056df7842825af3e62dce7351f8d62a6c
jhalmeko <joel.halmekoski@edu.lapinamk.fi>
2024-03-22 11:35:48 +0200

    Navigation drawer commit

--- AndroidCoolboxRyhma2/app/build.gradle.kts
+++ AndroidCoolboxRyhma2/app/build.gradle.kts
@@ -46,6 +46,10 @@ defaultConfig {
 
 dependencies {
 
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("io.coil-kt:coil-compose:2.6.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
--- AndroidCoolboxRyhma2/app/src/main/java/com/example/androidcoolboxryhma2/MainActivity.kt
+++ AndroidCoolboxRyhma2/app/src/main/java/com/example/androidcoolboxryhma2/MainActivity.kt
@@ -7,21 +7,61 @@ import androidx.compose.material3.Surface
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
+import androidx.fragment.app.FragmentManager
 
-class MainActivity : ComponentActivity() {
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        setContent {
-            val navController = rememberNavController()
+// Setup Navigation Drawer
+        drawerLayout = findViewById(R.id.drawer_layout)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
 
-            NavHost(navController = navController, startDestination = "loginScreen") {
-            composable("loginScreen"){
-                LoginScreen {
+        val appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        findViewById<NavigationView>(R.id.nav_view).setupWithNavController(navController)
 
-                }
-            }
-            }
+        // Setup ActionBarDrawerToggle
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, findViewById(R.id.toolbar),
+            R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
         }
     }
+
+    // setContent {
+    //    val navController = rememberNavController()
+
+    //    NavHost(navController = navController, startDestination = "loginScreen") {
+    //        composable("loginScreen"){
+    //            LoginScreen {
+
+    //            }
+
 }
