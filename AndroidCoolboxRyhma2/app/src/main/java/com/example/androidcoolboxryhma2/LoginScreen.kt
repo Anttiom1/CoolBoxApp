b046ad8c94c502d823e36243bf0ab36d1ae38047
Antti Mursu <Antti.Mursu@edu.lapinamk.fi>
2024-04-18 13:17:58 +0300

    lineGraph & QoL
    Added password view toggle

--- AndroidCoolboxRyhma2/app/src/main/java/com/example/androidcoolboxryhma2/LoginScreen.kt
+++ AndroidCoolboxRyhma2/app/src/main/java/com/example/androidcoolboxryhma2/LoginScreen.kt
@@ -9,14 +9,24 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.androidcoolboxryhma2.viewmodel.LoginViewModel
@@ -25,6 +35,7 @@ import com.example.androidcoolboxryhma2.viewmodel.LoginViewModel
 fun LoginScreen(goToCategoriesScreen: () -> Unit){
     val vm: LoginViewModel = viewModel()
     val context = LocalContext.current
+    var isPasswordVisible by remember { mutableStateOf(false) }
 
     LaunchedEffect(key1 = vm.loginState.value.error) {
         vm.loginState.value.error?.let {
@@ -58,10 +69,26 @@ Box(modifier = Modifier.fillMaxSize()) {
                 )
             Spacer(modifier = Modifier.height(8.dp))
             OutlinedTextField(
-                visualTransformation = PasswordVisualTransformation(),
+                visualTransformation = if (isPasswordVisible) VisualTransformation.None
+                    else PasswordVisualTransformation(),
                 value = vm.loginState.value.password,
                 onValueChange = { newPassword -> vm.setPassword(newPassword)},
-                label = {Text("Password")}
+                label = {Text("Password")},
+                trailingIcon = {
+                    //Button to change password visibility
+                    IconButton(
+                        onClick = {
+                            isPasswordVisible = !isPasswordVisible
+                        }
+                    ) {
+                        //Change icon and show password in clear text or as hidden
+                        Icon(imageVector = if (isPasswordVisible) ImageVector.vectorResource(R.drawable.baseline_visibility_24)
+                        else ImageVector.vectorResource(R.drawable.baseline_visibility_off_24)
+                            , contentDescription = if (isPasswordVisible) "Hide password"
+                            else "Show password"
+                        )
+                    }
+                }
             )
             Spacer(modifier = Modifier.height(8.dp))
             Button(
