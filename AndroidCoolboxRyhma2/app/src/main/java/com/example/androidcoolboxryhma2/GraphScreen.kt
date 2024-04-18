0b77cfa985e984c51d7def2ab68942f5c5034ffc
Antti Mursu <Antti.Mursu@edu.lapinamk.fi>
2024-04-18 15:44:17 +0300

    Update GraphScreen.kt
    changed dropdown menu to button toggle

--- AndroidCoolboxRyhma2/app/src/main/java/com/example/androidcoolboxryhma2/GraphScreen.kt
+++ AndroidCoolboxRyhma2/app/src/main/java/com/example/androidcoolboxryhma2/GraphScreen.kt
@@ -4,19 +4,26 @@ package com.example.androidcoolboxryhma2
 import android.text.Layout
 import android.util.Log
 import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
@@ -26,6 +33,8 @@ import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -44,6 +53,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Blue
 import androidx.compose.ui.graphics.Color.Companion.Green
 import androidx.compose.ui.graphics.Color.Companion.Red
@@ -51,6 +61,7 @@ import androidx.compose.ui.graphics.Color.Companion.Yellow
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.size.Dimension
 import com.example.androidcoolboxryhma2.viewmodel.GraphScreenViewModel
@@ -83,7 +94,8 @@ import kotlin.random.Random
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GraphScreen(goToHome: () -> Unit,
-              onMenuClick: () -> Unit) {
+                onMenuClick: () -> Unit,
+                juuhOnClick: (index: Int) -> Unit = {}) {
     val vm: GraphScreenViewModel = viewModel()
 
     val modelProducer = remember { ChartEntryModelProducer() }
@@ -188,42 +200,42 @@ fun GraphScreen(goToHome: () -> Unit,
     }
 
     Surface(modifier = Modifier.fillMaxSize()) {
-        Scaffold (
+        Scaffold(
             topBar = {
-                TopAppBar (
+                TopAppBar(
                     navigationIcon = {
                         IconButton(onClick = { onMenuClick() }) {
                             Icon(imageVector = Icons.Default.Menu, contentDescription = "Menu")
                         }
                     },
-                        title = {
-                    Text(text = "Graph")
-                },
+                    title = {
+                        Text(text = "Graph")
+                    },
                     actions = {
-                        IconButton(onClick = {goToHome()}) {
+                        IconButton(onClick = { goToHome() }) {
                             Icon(imageVector = Icons.Default.Home, contentDescription = "Home")
                         }
                     })
             }
         ) {
-            Column(
+            Card(
                 modifier = Modifier
-                    .padding(it)
-                    .padding(8.dp)
                     .fillMaxSize()
+                    .padding(it)
             ) {
-                Card(
-                    modifier = Modifier
-                        .fillMaxSize()
-                ) {
+                Column {
                     OutlinedTextField(value = vm.getDateString(),
-                        modifier = Modifier.padding(8.dp)
+                        modifier = Modifier
+                            .padding(8.dp)
                             .fillMaxWidth(),
                         onValueChange = { },
                         readOnly = true,
                         leadingIcon = {
                             IconButton(onClick = { openDialog.value = true }) {
-                                Icon(imageVector = Icons.Default.DateRange, contentDescription = "Date")
+                                Icon(
+                                    imageVector = Icons.Default.DateRange,
+                                    contentDescription = "Date"
+                                )
                             }
                         },
                         trailingIcon = {
@@ -235,7 +247,10 @@ fun GraphScreen(goToHome: () -> Unit,
                                     )
                                 }
                                 IconButton(onClick = { incrementDay() }) {
-                                    Icon(Icons.Default.KeyboardArrowRight, contentDescription = "Next")
+                                    Icon(
+                                        Icons.Default.KeyboardArrowRight,
+                                        contentDescription = "Next"
+                                    )
                                 }
                             }
                         }
@@ -253,10 +268,10 @@ fun GraphScreen(goToHome: () -> Unit,
                                     onClick = {
                                         openDialog.value = false
                                         vm.calculateDate(datePickerState.selectedDateMillis)
-                                        if (selectedOption == "Ulkolämpötila"){
+                                        if (selectedOption == "Ulkolämpötila") {
                                             vm.getDailyAverageTemperature()
                                         }
-                                        if (selectedOption == "Sähkönkulutus"){
+                                        if (selectedOption == "Sähkönkulutus") {
                                             vm.getDailyEnergyConsumption()
                                         }
 
@@ -279,94 +294,154 @@ fun GraphScreen(goToHome: () -> Unit,
                             DatePicker(state = datePickerState)
                         }
                     }
+                    val cornerRadius = 8.dp
 
-                        ExposedDropdownMenuBox(expanded = expanded, onExpandedChange = {expanded = !expanded }) {
-                            TextField(
-                                value = selectedOption,
-                                onValueChange = {},
-                                readOnly = true,
-                                trailingIcon = {ExposedDropdownMenuDefaults.TrailingIcon(
-                                    expanded = expanded)},
-                                modifier = Modifier.menuAnchor()
-                                    .padding(8.dp)
-                                    .fillMaxWidth()
-                            )
-                            ExposedDropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }
-                            ) {
-                                menuOptions.forEach { item ->
-                                    DropdownMenuItem(
-                                        text = { Text(text = item) },
-                                        onClick = {
-                                            selectedOption = item
-                                            if (selectedOption == "Sähkönkulutus" ){
-                                                vm.getDailyEnergyConsumption()
-                                            }
-                                            if (selectedOption == "Ulkolämpötila" ){
-                                                vm.getDailyAverageTemperature()
-                                            }
-                                            expanded = false
-                                        })
-                                    }
+                    val (selectedIndex, onIndexSelected) = remember { mutableStateOf<Int?>(null) }
+                    Row(
+                        modifier =
+                        Modifier.padding(8.dp)
+                            .fillMaxWidth()
+                    ) {
+                        menuOptions.forEachIndexed { index, item ->
+                            OutlinedButton(
+                                modifier = when (index) {
+                                    0 ->
+                                        Modifier
+                                            .offset(0.dp, 0.dp)
+                                            .zIndex(if (selectedIndex == index) 1f else 0f)
+
+                                    else ->
+                                        Modifier
+                                            .offset((-1 * index).dp, 0.dp)
+                                            .zIndex(if (selectedIndex == index) 1f else 0f)
                                 }
-                            }
-                            if (selectedOption == "Sähkönkulutus"){
-                                val marker = rememberMarker()
-                                Chart(
-                                    marker = marker,
-                                    startAxis = rememberStartAxis(
-                                        itemPlacer = AxisItemPlacer.Vertical.default(13),
-                                        valueFormatter = {value, _ ->
-                                            String.format("%.2f" , value) + "kWh"
-                                        }
-                                    ),
-                                    bottomAxis = rememberBottomAxis(
-                                        valueFormatter = {value, _ ->
-                                            value.toInt().toString()
-                                        }
-                                    ),
-                                    chartModelProducer = columnModelProducer,
-                                    modifier = Modifier.fillMaxSize(),
-                                    chart = lineChart(
-                                        axisValuesOverrider = AxisValuesOverrider.fixed(minY = 0f, maxY = 1f),
-                                        lines = datasetLineSpec,
-
+                                    .weight(1F).fillMaxWidth(),
+                                onClick = {
+                                    onIndexSelected(index)
+                                    juuhOnClick(index)
+                                    selectedOption = item
+                                    if (selectedOption == "Sähkönkulutus") {
+                                        vm.getDailyEnergyConsumption()
+                                    }
+                                    if (selectedOption == "Ulkolämpötila") {
+                                        vm.getDailyAverageTemperature()
+                                    }
+                                },
+                                shape = when (index) {
+                                    // left outer button
+                                    0 -> RoundedCornerShape(
+                                        topStart = cornerRadius,
+                                        topEnd = 0.dp,
+                                        bottomStart = cornerRadius,
+                                        bottomEnd = 0.dp
                                     )
+                                    // right outer button
+                                    menuOptions.size - 1 -> RoundedCornerShape(
+                                        topStart = 0.dp,
+                                        topEnd = cornerRadius,
+                                        bottomStart = 0.dp,
+                                        bottomEnd = cornerRadius
+                                    )
+                                    // middle button
+                                    else -> RoundedCornerShape(topStart = 0.dp, topEnd = 0.dp, bottomStart = 0.dp, bottomEnd = 0.dp)
+                                },
+                                border = BorderStroke(
+                                    1.dp, if (selectedIndex == index) {
+                                        MaterialTheme.colorScheme.primary
+                                    } else {
+                                        Color.DarkGray.copy(alpha = 0.75f)
+                                    }
+                                ),
+                                colors = if (selectedIndex == index) {
+                                    // selected colors
+                                    ButtonDefaults.outlinedButtonColors(
+                                        containerColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.1f),
+                                        contentColor = MaterialTheme.colorScheme.primary
+                                    )
+                                } else {
+                                    // not selected colors
+                                    ButtonDefaults.outlinedButtonColors(
+                                        containerColor = MaterialTheme.colorScheme.surface,
+                                        contentColor = MaterialTheme.colorScheme.primary
+                                    )
+                                },
+                            ) {
+                                Text(
+                                    text = item,
+                                    color = if (selectedIndex == index) {
+                                        MaterialTheme.colorScheme.primary
+                                    } else {
+                                        Color.DarkGray.copy(alpha = 0.9f)
+                                    },
+                                    modifier = Modifier.padding(horizontal = 8.dp)
                                 )
                             }
-                            if (selectedOption == "Ulkolämpötila"){
-                                val marker = rememberMarker()
-                                val label = TextComponent.Builder()
+                        }
+                    }
+                    if (selectedOption == "Sähkönkulutus") {
+                        val marker = rememberMarker()
+                        Chart(
+                            marker = marker,
+                            startAxis = rememberStartAxis(
+                                itemPlacer = AxisItemPlacer.Vertical.default(13),
+                                valueFormatter = { value, _ ->
+                                    String.format("%.2f", value) + "kWh"
+                                }
+                            ),
+                            bottomAxis = rememberBottomAxis(
+                                valueFormatter = { value, _ ->
+                                    value.toInt().toString()
+                                }
+                            ),
+                            chartModelProducer = columnModelProducer,
+                            modifier = Modifier.fillMaxSize(),
+                            chart = lineChart(
+                                axisValuesOverrider = AxisValuesOverrider.fixed(
+                                    minY = 0f,
+                                    maxY = 1f
+                                ),
+                                lines = datasetLineSpec,
 
-                                Chart(
-                                    modifier = Modifier.fillMaxSize(),
-                                    marker = marker,
-                                    chart = lineChart(
-                                        axisValuesOverrider = AxisValuesOverrider.fixed(minY = -25f, maxY = 25f),
-                                        lines = datasetLineSpec
-                                    ),
-                                    chartModelProducer = modelProducer,
-
-                                    startAxis = rememberStartAxis(
-                                        valueFormatter = { value, _ ->
-                                            value.round.toString() + "°C"
-                                        },
-                                        itemPlacer = AxisItemPlacer.Vertical.default(13)
-                                    ),
-
-                                    bottomAxis = rememberBottomAxis(
-                                        label = label.build(),
-                                        valueFormatter = { value, _ ->
-                                            value.toInt().toString()
-                                        },
-                                        guideline = null
-                                    ),
-                                    chartScrollState = scrollState,
-                                    isZoomEnabled = true
                                 )
-                            }
+                        )
+                    }
+                    if (selectedOption == "Ulkolämpötila") {
+                        val marker = rememberMarker()
+                        val label = TextComponent.Builder()
+
+                        Chart(
+                            modifier = Modifier.fillMaxSize(),
+                            marker = marker,
+                            chart = lineChart(
+                                axisValuesOverrider = AxisValuesOverrider.fixed(
+                                    minY = -25f,
+                                    maxY = 25f
+                                ),
+                                lines = datasetLineSpec
+                            ),
+                            chartModelProducer = modelProducer,
+
+                            startAxis = rememberStartAxis(
+                                valueFormatter = { value, _ ->
+                                    value.round.toString() + "°C"
+                                },
+                                itemPlacer = AxisItemPlacer.Vertical.default(13)
+                            ),
+
+                            bottomAxis = rememberBottomAxis(
+                                label = label.build(),
+                                valueFormatter = { value, _ ->
+                                    value.toInt().toString()
+                                },
+                                guideline = null
+                            ),
+                            chartScrollState = scrollState,
+                            isZoomEnabled = true
+                        )
                     }
                 }
             }
         }
+    }
     }
 
